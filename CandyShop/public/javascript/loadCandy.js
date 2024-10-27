724d8b41aa3f5ce742543c37f1b03b778c257d2b
katieb8976 <katiec8976@hotmail.com>
2024-10-27 12:18:54 -0400

    fixed something else

--- CandyShop/public/javascript/loadCandy.js
+++ CandyShop/public/javascript/loadCandy.js
@@ -1,11 +1,6 @@
 function getCandyList() {
     $.get("/getList", {}, updateCandy)
 }
-function getCandyList() {
-    console.log("getCandyList is running");  // Debugging log
-    $.get("/getList", {}, updateCandy);
-}
-
 
 //function updateImages(jsonList) {
  ///   console.log('jsonList', jsonList)
