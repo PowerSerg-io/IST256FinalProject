02a9737121980a383a00752e215dd39b09c7cf6e
katieb8976 <katiec8976@hotmail.com>
2024-11-05 14:16:25 -0500

    updated stuff

--- CandyShop/public/javascript/rating.js
+++ CandyShop/public/javascript/rating.js
@@ -8,6 +8,9 @@ function setList(data){
         var item = $('<button></button>').text(val.title)
         item.addClass('btn btn-primary mx-2')
         // item.click TODO
+        item.click(function(){
+            getRating(val)
+        })
         $('#leftNav').append(item)
     })
     // TODO load an initial trip
@@ -18,4 +21,9 @@ function setRating(data){
     $('#idx').html(data.idx)
     $('#title').html(data.title)
     $('#photo').attr('src', './images/' + data.image)
+    $('#date').html(data.date);
+    $('label:first').html(data.days);
+    $('label:first').html(data.location);
+    $('.star:gt('+ (parseInt(data.rating) -1) + ")")
+    .attr("src", "images/empty.png")
 }
