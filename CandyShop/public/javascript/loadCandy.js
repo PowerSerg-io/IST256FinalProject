203d647f7bc6b8dc1dd5dde0605ea13b97950cf4
Sergio <Sergio.ulloa.1271@gmail.com>
2024-11-10 16:23:57 -0500

    look at 54 in rating.js

--- CandyShop/public/javascript/loadCandy.js
+++ CandyShop/public/javascript/loadCandy.js
@@ -1,7 +1,23 @@
 function getCandyList() {
-    $.get("/getList", {}, updateCandy)
+    $.get("/getList")
+        .done(updateCandy)
+        .fail(function() {
+            alert('Failed to load the candy list.');
+        });
 }
 
-//function updateImages(jsonList) {
- ///   console.log('jsonList', jsonList)
- ///}
+function updateCandy(jsonList) {
+    console.log('jsonList', jsonList);
+
+    // Assuming 'jsonList' is an array of candy objects with properties like 'name' and 'image'
+    $('#candyContainer').empty();  // Clear previous items if needed
+    $.each(jsonList, function(index, candy) {
+        // Create an image element for each candy
+        var candyItem = $('<div class="candy-item"></div>');
+        var candyImage = $('<img>').attr('src', './images/' + candy.image).addClass('candy-image');
+        var candyName = $('<p></p>').text(candy.name).addClass('candy-name');
+
+        candyItem.append(candyImage).append(candyName);
+        $('#candyContainer').append(candyItem);
+    });
+}
--- CandyShop/public/javascript/rating.js
+++ CandyShop/public/javascript/rating.js
@@ -1,78 +1,87 @@
-function loadAllRatings(){
-    $.get('/getRatingsList', setList)
+function loadAllRatings() {
+    $.get('/getRatingsList')
+        .done(setList)
+        .fail(function() {
+            alert('Failed to load ratings list.');
+        });
 }
 
-function setList(data){
-    console.log('data: ', data)
+function setList(data) {
+    console.log('data: ', data);
     $.each(data, function(key, val) {
-        var item = $('<button></button>').text(val.title)
-        item.addClass('btn btn-primary mx-2')
-        // item.click TODO
-        item.click(function(){
-            setRating(val)
-        })
-        $('#leftNav').append(item)
-    })
-    // TODO load an initial trip
-    setRating(data[0])
+        var item = $('<button></button>').text(val.title);
+        item.addClass('btn btn-primary mx-2');
+        item.click(function() {
+            setRating(val);
+        });
+        $('#leftNav').append(item);
+    });
+
+    // Load an initial trip if data is not empty
+    if (data.length > 0) {
+        setRating(data[0]);
+    } else {
+        alert('No ratings available.');
+    }
 }
 
-function setRating(data){
-    $('#idx').html(data.idx)
-    $('#title').html(data.title)
-    $('#photo').attr('src', './images/' + data.image)
+function setRating(data) {
+    $('#idx').html(data.idx);
+    $('#title').html(data.title);
+    $('#photo').attr('src', './images/' + data.image);
     $('#date').html(data.date);
-    $('label:first').html(data.days);
-    $('label:first').html(data.location);
-    $('.star:gt('+ (parseInt(data.rating) -1) + ")")
-    .attr("src", "images/empty.png")
+    $('label#daysLabel').html(data.days);
+    $('label#locationLabel').html(data.location);
+
+    // Set up star rating display
+    $('.star').each(function(index) {
+        $(this).attr('src', index < data.rating ? './images/star.jpg' : './images/empty.png');
+
+        // Allow user to rate by clicking stars
+        $(this).off('click').click(function() {
+            var newRating = index + 1;  // Rating is 1-based
+            updateRating(newRating);
+        });
+    });
 }
 
-// function loadAllRatings(){
-//     $.get('/getRatingsList', setList);
-// }
-
-// function setList(data){
-//     console.log('data: ', data);
-//     $.each(data, function(key, val) {
-//         var item = $('<button></button>').text(val.title);
-//         item.addClass('btn btn-primary mx-2');
-//         item.click(function(){
-//             getRating(val);
-//         });
-//         $('#leftNav').append(item);
-//     });
-//     setRating(data[0]);  // Load the initial trip
-// }
-
-// function setRating(data){
-//     $('#idx').html(data.idx);
-//     $('#title').html(data.title);
-//     $('#photo').attr('src', './images/' + data.image);
-//     $('#date').html(data.date);
-//     $('label:first').html(data.days);
-//     $('label:last').html(data.location); // Fixed the location display
+function updateRating(rating) {
+    const currentIdx = $('#idx').text(); // Assuming this holds the ID or index of the current rating
+    const data = { rating: rating, idx: currentIdx };
     
-//     // Set up star rating
-//     $('.star').each(function(index) {
-//         $(this).attr('src', index < data.rating ? 'images/full.png' : 'images/empty.png');
+    console.log('Sending data:', data); // Log the data sent to server
 
-//         // Allow user to rate by clicking stars
-//         $(this).off('click').click(function() {
-//             var newRating = index + 1;  // Rating is 1-based
-//             updateRating(newRating);
-//         });
-//     });
-// }
+    $.post('/submitRating', data)
+        .done(function(response) {
+            console.log('Server response:', response);
+            if (response.success) {
+                alert('Rating submitted successfully!');
+                setRating(response.updatedData);  // Update display with new data
+            } else {
+                alert('Failed to submit rating. Response was not successful.');
+            }
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.error('Request failed:', textStatus, errorThrown);  // Log details of the failure
+            alert('Error submitting rating. Please try again.');
+        });
+}
 
-// function updateRating(rating) {
-//     $.post('/submitRating', { rating: rating }, function(response) {
-//         if (response.success) {
-//             alert('Rating submitted successfully!');
-//             setRating(response.updatedData);  // Optionally update the display
-//         } else {
-//             alert('Failed to submit rating.');
-//         }
-//     });
-// }
-//  21:35
+app.post('/submitRating', function(req, res) {
+    const { idx, rating } = req.body;
+
+    // Find the candy with the matching idx
+    const candy = rating.find(item => item.idx === idx);
+    if (candy) {
+        candy.rating = rating;  // Update the rating
+
+        // Write the updated ratings back to the file
+        fs.writeFile('./public/data/rating.json', JSON.stringify(rating, null, 2), (err) => {
+            if (err) return res.status(500).json({ success: false, error: 'Failed to save rating.' });
+            res.json({ success: true, updatedData: candy });
+        });
+    } else {
+        res.status(404).json({ success: false, error: 'Candy not found.' });
+    }
+});
+
